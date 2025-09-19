a5b9ca46acb5799191caa7765db84d2796ca1f12
Ali Al-Kaswan <alialkaswan@ace.ewi.tudelft.nl>
2025-09-19 11:58:06 +0200

    fix attack forg plen results

--- experiments/quantization/forgetting/attack_forg_plen/bf16_ep0.py
+++ experiments/quantization/forgetting/attack_forg_plen/bf16_ep0.py
@@ -52,19 +52,19 @@ gen_name_250 = p_name + epoch + plen[3]
 # perform the data extraction attacks
 gs_mem_100 = pipe(list(df['prefix_100']), batch_size=32)
 print('100 done')
-gs_mem_150 = pipe(list(df['prefix_100']), batch_size=32)
+gs_mem_150 = pipe(list(df['prefix_150']), batch_size=32)
 print('150 done')
-gs_mem_200 = pipe(list(df['prefix_100']), batch_size=32)
+gs_mem_200 = pipe(list(df['prefix_200']), batch_size=32)
 print('200 done')
-gs_mem_250 = pipe(list(df['prefix_100']), batch_size=32)
+gs_mem_250 = pipe(list(df['prefix_250']), batch_size=32)
 print('250 done')
 torch.cuda.empty_cache()
 
 # Save the results on each of the datasets
 df = df.add_column(gen_name_100, [gs_mem_100[i][0]['generated_text'][len(list(df['prefix_100'])[i]):]  for i,_ in enumerate(df)])
-df = df.add_column(gen_name_150, [gs_mem_150[i][0]['generated_text'][len(list(df['prefix_100'])[i]):]  for i,_ in enumerate(df)])
-df = df.add_column(gen_name_200, [gs_mem_200[i][0]['generated_text'][len(list(df['prefix_100'])[i]):]  for i,_ in enumerate(df)])
-df = df.add_column(gen_name_250, [gs_mem_250[i][0]['generated_text'][len(list(df['prefix_100'])[i]):]  for i,_ in enumerate(df)])
+df = df.add_column(gen_name_150, [gs_mem_150[i][0]['generated_text'][len(list(df['prefix_150'])[i]):]  for i,_ in enumerate(df)])
+df = df.add_column(gen_name_200, [gs_mem_200[i][0]['generated_text'][len(list(df['prefix_200'])[i]):]  for i,_ in enumerate(df)])
+df = df.add_column(gen_name_250, [gs_mem_250[i][0]['generated_text'][len(list(df['prefix_250'])[i]):]  for i,_ in enumerate(df)])
 
 # compute the evaluation metrics
 expath = './'
